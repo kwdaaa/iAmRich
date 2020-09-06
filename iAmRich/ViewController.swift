bdb8a581ba9dc086717452467ba999b86014c9d0
kwdaaa <kwd.aaa.a@icloud.com>
2020-09-06 18:44:45 +0900

    firstcommit

---
+++ iAmRich/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  iAmRich
+//
+//  Created by 川田 文香 on 2020/09/05.
+//  Copyright © 2020 kwdaaa.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+
